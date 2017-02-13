582a8aa3594759df69b68e25b0c3f7c160aae858
cent <cent@tokyo.densan-s.co.jp>
2017-02-13 15:06:25 +0900

    firstcommit

---
+++ src/main/java/com/densan/sample/SampleService.java
@@ -0,0 +1,22 @@
+package com.densan.sample;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("sample")
+public class SampleService {
+	final Logger logger = Logger.getLogger(this.getClass().getName());
+
+     @GET
+     @Path("{param}")
+     public Response getMsg(@PathParam("param") String msg) {
+          String output = "Jersey say2 : " + msg;
+          logger.info("logtest" + output);
+          return Response.status(200).entity(output).build();
+     }
+
+}
