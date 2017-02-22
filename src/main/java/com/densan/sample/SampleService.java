0d2c878f431b424e638376db18452a00f7af4a6a
cent <cent@dsktkosvr2.tokyo.densan-s.co.jp>
2017-02-22 13:38:00 +0900

    swagger

--- src/main/java/com/densan/sample/SampleService.java
+++ src/main/java/com/densan/sample/SampleService.java
@@ -7,12 +7,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
 @Path("sample")
+@Api(value = "/sample", description="test")
 public class SampleService {
 	final Logger logger = Logger.getLogger(this.getClass().getName());
 
      @GET
      @Path("{param}")
+     @ApiOperation(value = "param")
      public Response getMsg(@PathParam("param") String msg) {
           String output = "Jersey say3 : " + msg;
           logger.info("logtest" + output);
