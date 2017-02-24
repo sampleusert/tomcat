a81b9a25b5267cb9c07faddb59b53dfcff0483bc
densan <densan@dskc0199.tokyo.densan-s.co.jp>
2017-02-24 19:19:17 +0900

    constantsjava

--- src/main/java/com/densan/sample/constant/Constants.java
+++ src/main/java/com/densan/sample/constant/Constants.java
@@ -1,8 +1,16 @@
 package com.densan.sample.constant;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.ResourceBundle;
+
+
 
 public class Constants {
 	
@@ -12,15 +20,23 @@ public class Constants {
 	 * @throws IOException
 	 */
 	public Properties getConstants() throws IOException {
-		
-    	//InputStream is = Constants.class.getClassLoader().getResourceAsStream("conf.properties");
-    	InputStream is = this.getClass().getResource("conf.properties").openStream();
+		Class c = this.getClass();
+    	InputStream is = c.getResourceAsStream("conf.properties");
+    	//InputStream is = c.getResource("conf.properties").openStream();
     	Properties properties = new Properties();
     	properties.load(is);
     	is.close();
     	
     	return properties;
-		
 	}
+	
+	public ResourceBundle getConstants2() throws IOException {
+		
+		File dicDir = Paths.get("/tmp").toFile();
+		URLClassLoader urlLoader = new URLClassLoader(new URL[]{dicDir.toURI().toURL()});
+		ResourceBundle myResource = ResourceBundle.getBundle("conf", Locale.getDefault(), urlLoader);
+		return myResource;
+		
+	}	
 
 }
